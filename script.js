9528ffb6e9bde2732340553c78a03767b0e66f9f
Elite Robust Ontology <eroinnovations9@gmail.com>
2025-07-24 16:28:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-// Smooth scroll (optional with Bootstrap)
-$('a[href^="#"]').on('click', function(e) {
-  e.preventDefault();
-  const target = $($(this).attr('href'));
-  if (target.length) {
-    $('html, body').animate({
-      scrollTop: target.offset().top - 70
-    }, 600);
-  }
+const menuToggle = document.getElementById('menu-toggle');
+const navList = document.getElementById('nav-list');
+
+menuToggle.addEventListener('click', () => {
+    navList.classList.toggle('show');
 });
